b5fa3efb5141a9e8df447a4cf2f6d118d7bdf762
Sanjay <ksanjayk@protonmail.com>
2025-02-02 23:48:21 +0530

    created Endpoints using extension methods to extend WebApplication

---
+++ julio-casal/GameStore/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,60 @@
+using System;
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGames";
+
+    // Its readonly because we are never going to create the entire List from scratch
+    private static readonly List<GameDto> games = [
+        new (1, "Elden Ring", "RPG", 59.99m, new DateOnly(2022, 2, 25)),
+        new (2, "God of War", "Action", 49.99m, new DateOnly(2018, 4, 20)),
+        new (3, "Cyberpunk 2077", "Action RPG", 39.99m, new DateOnly(2020, 12, 10)),
+        new (4, "The Witcher 3", "RPG", 29.99m, new DateOnly(2015, 5, 19))
+    ];
+
+    public static WebApplication MapGamesEndpoints(this WebApplication app)
+    {
+        // GET /games
+        app.MapGet("games", ()=> games);
+
+        // GET by ID /games/id
+        app.MapGet("games/{id}",(int id) =>{ 
+            GameDto? game = games.Find(game => game.Id == id);
+                return game is null ? Results.NotFound() : Results.Ok(game);
+            })
+            .WithName(GetGameEndpointName);
+
+        // POST /games
+        app.MapPost("games", (CreateDto newGame)=>{
+            GameDto game = new(games.Count +1 , newGame.Name, newGame.Genre,  newGame.Price, newGame.ReleaseDate);
+            games.Add(game);
+            
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id= game.Id}, game);
+        });
+
+        // PUT /games
+        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame)=>{
+            var index = games.FindIndex(game=> game.Id == id);
+
+            if(index ==-1){
+            return Results.NotFound(); 
+            }
+
+            games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
+            return Results.NoContent();
+        });
+
+        // DELETE /games/1
+        app.MapDelete("games/{id}", (int id)=>{
+            games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+
+        return app;
+
+    }
+
+}
--- julio-casal/GameStore/GameStore.Api/Program.cs
+++ julio-casal/GameStore/GameStore.Api/Program.cs
@@ -1,45 +1,8 @@
 using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGames";
-
-List<GameDto> games = [
-    new (1, "Elden Ring", "RPG", 59.99m, new DateOnly(2022, 2, 25)),
-    new (2, "God of War", "Action", 49.99m, new DateOnly(2018, 4, 20)),
-    new (3, "Cyberpunk 2077", "Action RPG", 39.99m, new DateOnly(2020, 12, 10)),
-    new (4, "The Witcher 3", "RPG", 29.99m, new DateOnly(2015, 5, 19))
-];
-
-// GET /games
-app.MapGet("games", ()=> games);
-
-// GET by ID /games/id
-app.MapGet("games/{id}",(int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpointName);
-
-// POST /games
-app.MapPost("games", (CreateDto newGame)=>{
-    GameDto game = new(games.Count +1 , newGame.Name, newGame.Genre,  newGame.Price, newGame.ReleaseDate);
-    games.Add(game);
-    
-    return Results.CreatedAtRoute(GetGameEndpointName, new {id= game.Id}, game);
-});
-
-// PUT /games
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame)=>{
-    var index = games.FindIndex(game=> game.Id == id);
-
-    games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
-    return Results.NoContent();
-});
-
-// DELETE /games/1
-app.MapDelete("games/{id}", (int id)=>{
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent();
-});
-
-
+app.MapGamesEndpoints();
 app.Run();
