f8eb17ac2b1da36273a152b646e4b2b3cf659451
Sanjay <ksanjayk@protonmail.com>
2025-02-02 22:44:10 +0530

    basic crud

--- julio-casal/GameStore/GameStore.Api/Program.cs
+++ julio-casal/GameStore/GameStore.Api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGames";
+
 List<GameDto> games = [
     new (1, "Elden Ring", "RPG", 59.99m, new DateOnly(2022, 2, 25)),
     new (2, "God of War", "Action", 49.99m, new DateOnly(2018, 4, 20)),
@@ -12,6 +14,32 @@ List<GameDto> games = [
 
 // GET /games
 app.MapGet("games", ()=> games);
+
+// GET by ID /games/id
+app.MapGet("games/{id}",(int id) => games.Find(game => game.Id == id))
+    .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("games", (CreateDto newGame)=>{
+    GameDto game = new(games.Count +1 , newGame.Name, newGame.Genre,  newGame.Price, newGame.ReleaseDate);
+    games.Add(game);
+    
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id= game.Id}, game);
+});
+
+// PUT /games
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame)=>{
+    var index = games.FindIndex(game=> game.Id == id);
+
+    games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id)=>{
+    games.RemoveAll(game => game.Id == id);
+    return Results.NoContent();
+});
 
 
 app.Run();
