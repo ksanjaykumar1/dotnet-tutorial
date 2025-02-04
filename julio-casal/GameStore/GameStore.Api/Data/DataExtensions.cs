23cef1a895cce7b12008309cc0b8b1d64841a144
Sanjay <ksanjayk@protonmail.com>
2025-02-04 23:18:54 +0530

    all DB operations converted to async

--- julio-casal/GameStore/GameStore.Api/Data/DataExtensions.cs
+++ julio-casal/GameStore/GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -7,13 +8,13 @@ namespace GameStore.Api.Data;
 database migrations automatically when the application starts.*/
 public static class DataExtensions
 {
-    public static void MigrateDb( this WebApplication app)
+    public static async Task MigrateDbAsync( this WebApplication app)
     {
         /* Creates a service scope to resolve dependencies 
         from the dependency injection (DI) container.*/
         using var scope = app.Services.CreateScope();
         /*Retrieves an instance of GameStoreContext from the DI container.*/
         var dbContext =  scope.ServiceProvider.GetRequiredService<GameStoreContext>();
-        dbContext.Database.Migrate();
+        await dbContext.Database.MigrateAsync();
     }
 }
